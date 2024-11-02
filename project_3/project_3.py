be81f477a46eab9d4235486be4edfd9963b67c39
Gabi363 <g.zmuda@novitas.com.pl>
2024-11-02 17:43:31 +0100

    znajdowanie orzeczenia i podmiotu

--- project_3/project_3.py
+++ project_3/project_3.py
@@ -1,5 +1,8 @@
 import spacy
+import textacy
+import pandas as pd
 from pypdf import PdfReader
+
 
 def pdf_to_string(file_path):
     reader = PdfReader(file_path)
@@ -9,13 +12,11 @@ def pdf_to_string(file_path):
     return text
 
 
-file_path = "Tolkien.pdf"
-pdf_text = pdf_to_string(file_path)
-print(pdf_text)
+# file_path = "Tolkien.pdf"
+# pdf_text = pdf_to_string(file_path)
+# print(pdf_text)
 
 nlp = spacy.load("en_core_web_sm")
-
-
 
 text = ("When Sebastian Thrun started working on self-driving cars at "
         "Google in 2007, few people outside of the company took him "
@@ -25,8 +26,14 @@ text = ("When Sebastian Thrun started working on self-driving cars at "
         "this week.")
 doc = nlp(text)
 
-print("Noun phrases:", [chunk.text for chunk in doc.noun_chunks])
-print("Verbs:", [token.lemma_ for token in doc if token.pos_ == "VERB"])
+svo_list = list(textacy.extract.subject_verb_object_triples(doc))
+vo_list = []
+for svo in svo_list:
+    v = svo[1][-1].lemma_
+    objects = " ".join(str(x) for x in svo[2])
+    o = list(nlp(objects).noun_chunks)[0].root.text
+    vo_list.append((v, o))
 
-for entity in doc.ents:
-    print(entity.text, entity.label_)
+for x in vo_list:
+    print(x)
+
