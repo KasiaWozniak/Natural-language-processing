9e98c7eb8e17596ecbb71a01683fe797ea477729
KasiaWozniak <115621435+KasiaWozniak@users.noreply.github.com>
2024-11-02 16:37:32 +0100

    pdf to string

--- project_3/project_3.py
+++ project_3/project_3.py
@@ -1,6 +1,21 @@
 import spacy
+from pypdf import PdfReader
+
+def pdf_to_string(file_path):
+    reader = PdfReader(file_path)
+    text = ""
+    for page in reader.pages:
+        text += page.extract_text()
+    return text
+
+
+file_path = "Tolkien.pdf"
+pdf_text = pdf_to_string(file_path)
+print(pdf_text)
 
 nlp = spacy.load("en_core_web_sm")
+
+
 
 text = ("When Sebastian Thrun started working on self-driving cars at "
         "Google in 2007, few people outside of the company took him "
