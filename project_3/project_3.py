5f70118f5c08a7db61e33e88d246357fe18eb6bf
Gabi363 <g.zmuda@novitas.com.pl>
2024-11-02 18:21:48 +0100

    gotowy program

--- project_3/project_3.py
+++ project_3/project_3.py
@@ -3,13 +3,6 @@ import textacy
 
 nlp = spacy.load("en_core_web_sm")
 
-text = ("When Sebastian Thrun started working on self-driving cars at "
-        "Google in 2007, few people outside of the company took him "
-        "seriously. “I can tell you very senior CEOs of major American "
-        "car companies would shake my hand and turn away because I wasn’t "
-        "worth talking to,” said Thrun, in an interview with Recode earlier "
-        "this week.")
-
 with open('Tolkien.txt', 'r', encoding="utf8") as file:
     text = file.read()
 
@@ -21,19 +14,16 @@ vo_list = []
 for svo in svo_list:
     v = svo[1][-1].lemma_
     objects = " ".join(str(x) for x in svo[2])
-    o = list(nlp(objects).noun_chunks)[0].root.text
-    vo_list.append((v, o))
-
-# vo_list.append(('start', 'game'))
-for x in vo_list:
-    print(x)
+    o_list = list(nlp(objects).noun_chunks)
+    if len(o_list) > 0:
+        o = o_list[0].root.text
+        vo_list.append((v, o))
 
 verbs_and_nouns = dict()
 for v, o in vo_list:
     verbs_and_nouns.setdefault(v, []).append(o)
-print(verbs_and_nouns)
 
-with open('verbs_and_nouns.txt', 'w') as file:
+with open('verbs_and_nouns.txt', 'w', encoding="utf8") as file:
     for v, o in verbs_and_nouns.items():
         o_string = ' '.join(o)
         file.write(f'{v}: {o_string}\n')
